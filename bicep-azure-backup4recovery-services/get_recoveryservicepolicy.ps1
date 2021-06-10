630b2d855872ece377d62a0a5b31e28f425810c0
YamamotoToshio <75714926+YamamotoToshio@users.noreply.github.com>
2021-06-10 16:26:12 +0900

    Delete bicep-azure-backup4recovery-services directory

--- bicep-azure-backup4recovery-services/get_recoveryservicepolicy.ps1
+++
@@ -1,8 +0,0 @@
-$resourceGroupName = "bicepsample0427-2"
-$recoveryServiceName = "sampleRecoveryContainer0"
-
-
-$vault = Get-AzRecoveryServicesVault -ResourceGroupName $resourceGroupName -Name $recoveryServiceName
-$policies = Get-AzRecoveryServicesBackupProtectionPolicy -VaultId $vault.id
-
-$policies | % { Write-Host $_.Id }
